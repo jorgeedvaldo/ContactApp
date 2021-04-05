a43d18ec3478866c091b9b0c470cb04edcc0a221
jorgeedvaldo <edvaldojorge1996@gmail.com>
2021-04-05 09:46:58 +0000

    Criando formulário simples

--- resources/views/livewire/contact-create.blade.php
+++ resources/views/livewire/contact-create.blade.php
@@ -1,5 +1,46 @@
+<h2>Criar Contato</h2>
+
 <div>
-    <div>Hello World - Please, digit your name</div>
-    <h5>{{$name}}</h5>
-    <input type="text" wire:model="name" style="border: 1px solid #000">
+    <form method="post">
+
+        <div>
+            <label>Nome</label>
+
+            <input type="text">
+
+            <div class="error-message">
+                @error('name')
+                    {{$message}}
+                @enderror
+            </div>
+        </div>
+
+        <div>
+            <label>Email</label>
+
+            <input type="email">
+
+            <div class="error-message">
+                @error('email')
+                    {{$message}}
+                @enderror
+            </div>
+        </div>
+
+        <div>
+            <label>Telefone</label>
+
+            <input type="text">
+
+            <div class="error-message">
+                @error('phone')
+                    {{$message}}
+                @enderror
+            </div>
+        </div>
+
+        <button type="submit">
+            Adicionar Contato
+        </button>
+    </form>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,44 @@
         @livewireStyles
 
         <style>
-            body {
-                font-family: 'Nunito';
+           body {
+                font-family: "Nunito";
+                font-size: 16px;
+            }
+            div.container {
+                width: 78%;
+                margin: 0 auto;
+            }
+            form div, form label, form input {
+                display:block;
+            }
+
+            form div {
+                margin-bottom: 10px;
+            }
+
+            form input {
+                padding: 5px;
+                width: 100%;
+                border-radius: 5px;
+                border: none;
+                border-bottom: 1px solid #ccc;
+                outline: none;
+            }
+
+            form button {
+                padding: 10px 15px;
+                background: #036;
+                border: 1px solid #000;
+                border-radius: 5px;
+                color: #fff;
+                font-weight: bold;
+            }
+
+            div.error-message {
+                color: #f00;
+                font-weight: bold;
+                padding-top: 4px;
             }
         </style>
     </head>
